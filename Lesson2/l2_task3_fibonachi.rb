075dc6ea9cdeaa75fe230089a2ac4bc16544bbfe
DmitriKudriashov <kds.120731@gmail.com>
2018-07-22 20:50:46 +0300

    Update l2_task3_fibonachi.rb

--- Lesson2/l2_task3_fibonachi.rb
+++ Lesson2/l2_task3_fibonachi.rb
@@ -2,7 +2,7 @@
 fibbonacci = [ 0, 1, 1 ]
 element = 2
 while element < 100
- fibbonacci << element
- element = fibbonacci[-1] + fibbonacci[-2]
+  fibbonacci << element
+  element = fibbonacci[-1] + fibbonacci[-2]
 end
 puts fibbonacci
